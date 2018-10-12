be1c5d3a18b8e22985f64fea409a68f717506cd1
Motola <Motola@DESKTOP-MIKJ13K>
2018-10-12 13:05:30 +0300

    add controller books

---
+++ Library/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Display(Name = "Release Date")]
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        [Display(Name = "Genre")]
+        public int GenreId { get; set; }
+        [ForeignKey("GenreId")]
+        public Genre Genre { get; set; }
+    }
+}
