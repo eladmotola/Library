194579a927a319cb24ee668aff26f197018e171c
Motola <Motola@DESKTOP-MIKJ13K>
2018-10-14 22:32:42 +0300

    add validator for the book author

--- Library/Models/Book.cs
+++ Library/Models/Book.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
 
 namespace Library.Models
 {
-    public class Book
+    public class Book : IValidatableObject
     {
         [Key]
         public int Id { get; set; }
@@ -23,5 +25,15 @@ namespace Library.Models
         public int GenreId { get; set; }
         [ForeignKey("GenreId")]
         public Genre Genre { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Regex.Matches(Author, @"[a-zA-Z]").Count != Author.Length)
+            {
+                yield return
+                  new ValidationResult(errorMessage: "Author contains only letters",
+                                       memberNames: new[] { "Author" });
+            }
+        }
     }
 }
