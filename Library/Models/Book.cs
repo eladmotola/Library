6b3707d0458ed2a2dcafbe6e924be26d0410695e
Motola <Motola@DESKTOP-MIKJ13K>
2018-10-14 22:09:27 +0300

    fix date in books at edit page

--- Library/Models/Book.cs
+++ Library/Models/Book.cs
@@ -15,6 +15,8 @@ namespace Library.Models
         public string Author { get; set; }
         [Display(Name = "Release Date")]
         [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         [Required]
         [Display(Name = "Genre")]
